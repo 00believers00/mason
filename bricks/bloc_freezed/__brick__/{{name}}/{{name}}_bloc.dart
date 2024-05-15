434abb4ee2af030c886710934d5edda3dec4bd92
Atiwat Bank Promarin <np.nawin@gmail.com>
2024-05-15 22:33:53 +0700

    feat: init

---
+++ bricks/bloc_freezed/__brick__/{{name}}/{{name}}_bloc.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+
+part '{{name}}_event.dart';
+part '{{name}}_state.dart';
+part '{{name}}_bloc.freezed.dart';
+
+class {{name.pascalCase()}}Bloc extends Bloc<{{name.pascalCase()}}Event, {{name.pascalCase()}}State>{
+  {{name.pascalCase()}}Bloc():super(const _Initial()){
+    on<{{name.pascalCase()}}Event>((event, emit){
+
+    });
+  }
+}
